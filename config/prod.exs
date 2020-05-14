6f2b38b2690fb7942b02d6d3a6f093d8ea21766a
喻林 <yulin_yl@126.com>
2020-05-14 10:50:37 +0800

    deploy

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :chat, ChatWeb.Endpoint,
   load_from_system_env: true,
-  http: [port: {:system, "PORT"}], #
-  url: [scheme: "https", host: "phxchat.herokuapp.com", port: 443],
+  #
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "phoenix-chat-minmax.herokuapp.com", port: 443],
   # port: System.get_env("PORT")], # Heroku Supplies the TCP Port
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
